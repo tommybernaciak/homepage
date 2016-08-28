df6d2b768986e952d68c1d85fb2a245c69c50cee
tommybernaciak <tommybernaciak@gmail.com>
2016-08-28 12:51:22 +0100

    delete homepage controller, add admin page, init sections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :http_basic_auth, only: :admin
+
+  def mainpage
+    @sections = Section.all
+  end
+
+  def admin
+    @sections = Section.all
+  end
+
+  private
+
+  def http_basic_auth
+    unless authenticate_with_http_basic { |user, password| user == Rails.application.secrets.admin_user && password == Rails.application.secrets.admin_pass }
+      request_http_basic_authentication
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root 'homepage#index'
+  root 'application#mainpage'
+  get '/admin', to: 'application#admin'
 end
