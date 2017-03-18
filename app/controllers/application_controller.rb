2e8740cb370d71cec10197bff133eb0638a19eac
tommybernaciak <tommybernaciak@gmail.com>
2017-03-18 20:52:48 +0000

    delete auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :http_basic_auth
 
   def me
     @title = action_name.humanize
-  end
-
-  private
-
-  # TODO: delete this method on blog launch
-  def http_basic_auth
-    if Rails.env.production?
-      unless authenticate_with_http_basic do |user, password|
-          user == Rails.application.secrets.admin_user && password == Rails.application.secrets.admin_pass
-        end
-        request_http_basic_authentication
-      end
-    end
   end
 end
