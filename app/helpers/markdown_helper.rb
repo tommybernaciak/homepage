9ce0d77883e6f788c62fa240fb24ebc4024d1db8
tommybernaciak <tommybernaciak@gmail.com>
2017-07-30 19:56:12 +0100

    colorize markdown, add support

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,22 +1,32 @@
 module MarkdownHelper
+  RENDER = Redcarpet::Render::HTML
 
-  def to_markdown(text)
-    options = { hard_wrap: true,
-                filter_html: true,
-                autolink: true,
-                no_intra_emphasis: true,
-                fenced_code_blocks: true,
-                gh_blockcode: true
-              }
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(text)
+  def render_markdown(text)
+    markdown = Redcarpet::Markdown.new(RENDER, options).render(text)
     syntax_highlighter(markdown).html_safe
   end
 
   def syntax_highlighter(html)
     doc = Nokogiri::HTML(html)
-    doc.search("//code[@lang]").each do |pre|
-      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-    end
+    doc.css('code').each { |code| colorize(code) }
     doc.to_s
+  end
+
+  def colorize(code)
+    return unless code[:class]
+    div = CodeRay.scan(code.text.rstrip, code[:class].to_sym).div
+    code = code.replace(div)
+    code.first.parent.swap(code.first)
+  end
+
+  def options
+    @options = {
+      hard_wrap: true,
+      filter_html: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      gh_blockcode: true
+    }
   end
 end
